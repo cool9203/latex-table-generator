3f624154a6183b18baa8d5ad0d5aee7e84f63a88
ychsu <ychsu@iii.org.tw>
2024-12-26 18:04:33 +0800

    Update: `paste_image_with_table_bbox` and `draw_table_bbox`, and fix np read image error

--- src/latex_table_generator/base.py
+++ src/latex_table_generator/base.py
@@ -2,7 +2,7 @@
 
 import io
 from pathlib import Path
-from typing import List, Union
+from typing import List, Tuple, Union
 
 import cv2
 import numpy as np
@@ -11,6 +11,42 @@ from img2table.tables.objects.extraction import ExtractedTable
 from PIL import Image as PILImage
 
 InputType = Union[str, Path, bytes, io.BytesIO, MatLike, PILImage.Image]
+
+
+def paste_image_with_table_bbox(
+    src: InputType,
+    dst: InputType,
+    table: ExtractedTable,
+    **kwds,
+) -> MatLike:
+    img = get_image(src=src)
+
+    x1 = max(table.bbox.x1, 0)
+    x2 = x1 + dst.shape[1]
+    y1 = max(table.bbox.y1, 0)
+    y2 = y1 + dst.shape[0]
+    img[y1:y2, x1:x2] = dst
+
+    return img
+
+
+def draw_table_bbox(
+    src: InputType,
+    tables: List[ExtractedTable],
+    margin: int,
+    color: Tuple[int, int, int] = (255, 255, 255),
+    **kwds,
+) -> MatLike:
+    img = get_image(src=src)
+
+    for table in tables:
+        x1 = max(table.bbox.x1 - margin, 0)
+        x2 = min(table.bbox.x2 + margin, img.shape[1])
+        y1 = max(table.bbox.y1 - margin, 0)
+        y2 = min(table.bbox.y2 + margin, img.shape[0])
+        img = cv2.rectangle(img, (x1, y1), (x2, y2), color, -1)
+
+    return img
 
 
 def crop_table_bbox(
@@ -39,7 +75,7 @@ def get_image(
     if isinstance(src, (MatLike, np.ndarray)):
         img = src
     elif isinstance(src, PILImage.Image):
-        img = np.asarray(src)
+        img = np.asarray(src).astype(np.uint8)  # Reference: https://github.com/opencv/opencv/issues/24522#issuecomment-1972141659
     else:
         if isinstance(src, bytes):
             _src = src
