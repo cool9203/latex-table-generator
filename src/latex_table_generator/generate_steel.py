44bef0eb622127708d2d2f9ea1e9b611e6cc0b08
cool9203 <octer18@gmail.com>
2025-01-20 13:35:04 +0800

    Add: generate steel

---
+++ src/latex_table_generator/generate_steel.py
@@ -0,0 +1,219 @@
+# coding: utf-8
+
+import json
+import logging
+import random
+from dataclasses import dataclass, field
+from os import PathLike
+from pathlib import Path
+from typing import List, Sequence, Tuple, Union
+
+from PIL import Image, ImageDraw, ImageFont
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel("INFO")
+
+
+def _get_fit_font_size(
+    text: str,
+    size: Tuple[int, int, int, int],
+    font: str,
+) -> ImageFont:
+    # Edit from: https://stackoverflow.com/a/61891053
+    font_size = 1
+    jump_size = 75
+    _font = ImageFont.truetype(font, font_size)
+    while True:
+        text_bbox = _font.getbbox(text)
+        logger.debug(f"text_bbox: {text_bbox}")
+        logger.debug(f"size: {size}")
+        logger.debug("-" * 25)
+
+        if text_bbox[3] < (size[3] - size[1]) and text_bbox[2] < (size[2] - size[0]):
+            font_size += jump_size
+        else:
+            jump_size = jump_size // 2
+            font_size -= jump_size
+        _font = ImageFont.truetype(font, font_size)
+
+        if jump_size <= 1:
+            break
+    return _font
+
+
+@dataclass
+class SteelRole:
+    x1: float
+    x2: float
+    y1: float
+    y2: float
+    angle: bool = False
+    range: Sequence[Union[int, float]] = field(default_factory=[1, 1000])
+    choices: Sequence[str] = field(default_factory=[])
+    before_choices: Sequence[str] = field(default_factory=[])
+    after_choices: Sequence[str] = field(default_factory=[])
+
+
+@dataclass
+class Steel:
+    origin_image: Image.Image
+    image: Image.Image
+    role_path: PathLike
+    image_path: PathLike
+    rotate_angle: float = 0.0
+    roles: List[SteelRole] = field(default_factory=[])
+    rng: random.Random = None
+
+    def __init__(
+        self,
+        image_path: PathLike,
+        role_path: PathLike = None,
+        rotate_angle: float = 0.0,
+        rng: random.Random = None,
+    ):
+        self.image_path = Path(image_path)
+        self.roles = list()
+        if role_path:
+            self.role_path = Path(role_path)
+        elif Path(self.image_path.parent, f"{self.image_path.stem}.txt").exists():
+            self.role_path = Path(self.image_path.parent, f"{self.image_path.stem}.txt")
+        elif Path(self.image_path.parent, f"{self.image_path.stem}.json").exists():
+            self.role_path = Path(self.image_path.parent, f"{self.image_path.stem}.json")
+        with self.role_path.open("r", encoding="utf-8") as f:
+            data = json.load(f)
+        for role in data.get("roles"):
+            self.roles.append(SteelRole(**role))
+
+        image = Image.open(image_path)
+        self.origin_image = image
+        self.image = self.hollow_image(image)
+        self.rotate_angle = rotate_angle
+        self.rng = rng if rng else random.Random()
+
+    def hollow_image(
+        self,
+        image: Image.Image,
+    ) -> Image.Image:
+        _image = image.copy()
+        draw_image = ImageDraw.Draw(_image)
+        for role in self.roles:
+            draw_image.rectangle(((role.x1, role.y1), (role.x2, role.y2)), fill=(255, 255, 255))
+        del draw_image
+        return _image
+
+    def draw_bbox(
+        self,
+        roles: List[SteelRole] = None,
+    ) -> Image.Image:
+        roles = roles if roles else self.roles
+        image = self.origin_image.copy()
+        draw_image = ImageDraw.Draw(image)
+        for index, role in enumerate(roles):
+            draw_image.text((role.x1, role.y1), str(index), fill=(0, 0, 0))
+            draw_image.rectangle(((role.x1, role.y1), (role.x2, role.y2)), outline="red")
+        del draw_image
+        return image
+
+    def generate(
+        self,
+        roles: List[SteelRole] = None,
+        font: str = "mingliu.ttc",
+        font_color: Union[str, Tuple[int, int, int]] = (0, 0, 0),
+        rng: random.Random = None,
+    ) -> Tuple[Image.Image, str]:
+        rng = rng if rng else self.rng
+        roles = roles if roles else self.roles
+
+        draw_image = ImageDraw.Draw(self.image)
+        label_texts = list()
+        for role in roles:
+            text = ""
+            if role.before_choices:
+                text += role.before_choices[rng.randint(0, len(role.before_choices) - 1)]
+
+            ranges = [n for n in range(*role.range)]
+            text += str(ranges[rng.randint(0, len(ranges) - 1)])
+            if role.angle:
+                text += "°"
+
+            if role.after_choices:
+                text += role.after_choices[rng.randint(0, len(role.after_choices) - 1)]
+
+            _font = _get_fit_font_size(
+                text=text,
+                size=(role.x1, role.y1, role.x2, role.y2),
+                font=font,
+            )
+            draw_image.text((role.x1, role.y1), text, fill=font_color, font=_font)
+            label_texts.append(text)
+        del draw_image
+        return (self.image, " ".join(label_texts))
+
+    def random_generate(
+        self,
+        roles: List[SteelRole] = None,
+        font: str = "mingliu.ttc",
+        font_color: Union[str, Tuple[int, int, int]] = (0, 0, 0),
+        rng: random.Random = None,
+        fraction: float = 0.5,
+    ) -> Tuple[Image.Image, str]:
+        rng = rng if rng else self.rng
+        roles = roles if roles else self.roles
+
+        draw_image = ImageDraw.Draw(self.image)
+        label_texts = list()
+        for role in roles:
+            text = ""
+            if role.before_choices:
+                text += role.before_choices[rng.randint(0, len(role.before_choices) - 1)]
+
+            ranges = [n for n in range(*role.range)]
+            text += str(ranges[rng.randint(0, len(ranges) - 1)])
+            if role.angle:
+                text += "°"
+
+            if role.after_choices:
+                text += role.after_choices[rng.randint(0, len(role.after_choices) - 1)]
+
+            start_x = role.x1 + rng.randint(0, int((role.x2 - role.x1) * fraction))
+            start_y = role.y1 + rng.randint(0, int((role.y2 - role.y1) * fraction))
+            _font = _get_fit_font_size(
+                text=text,
+                size=(start_x, start_y, role.x2, role.y2),
+                font=font,
+            )
+            draw_image.text((start_x, start_y), text, fill=font_color, font=_font)
+            label_texts.append(text)
+        return (self.image, " ".join(label_texts))
+
+
+def load_steel(
+    image_path: PathLike,
+    _extensions: List[str] = ["png", "jpg", "jpeg"],
+    rng: random.Random = None,
+) -> List[Steel]:
+    rng = rng if rng else random.Random()
+    image_paths = list()
+    for _extension in _extensions:
+        image_paths += [p for p in Path(image_path).glob(f"*.{_extension.lower()}")]
+        image_paths += [p for p in Path(image_path).glob(f"*.{_extension.upper()}")]
+
+    steels = list()
+    for image_path in image_paths:
+        steels.append(Steel(image_path, rng=rng))
+    return steels
+
+
+if __name__ == "__main__":
+    rng = random.Random(10)
+    steels = load_steel(
+        image_path="./images/steels",
+        rng=rng,
+    )
+
+    for steel in steels:
+        (image, label) = steel.generate()
+        logger.debug(label)
+        image.show()
+        input()
