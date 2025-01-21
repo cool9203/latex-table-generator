8e3ca8add5221c565db2a24bacb76f9493a7ea6a
cool9203 <octer18@gmail.com>
2025-01-21 09:58:47 +0800

    Refactor: process latex string method move to `utils.py`

--- src/latex_table_generator/utils.py
+++ src/latex_table_generator/utils.py
@@ -1,11 +1,23 @@
 # coding: utf-8
 
 import logging
+import re
 from os import PathLike
 from pathlib import Path
-from typing import List, Sequence, Union
+from typing import List, Sequence, Tuple, Union
 
 import numpy as np
+import pandas as pd
+
+from latex_table_generator.errors import (
+    NotLatexError,
+    NotSupportMulticolumnLatexError,
+    NotSupportMulticolumnLatexTableError,
+)
+
+_latex_table_begin_pattern = r"\\begin{tabular}{.*}"
+_latex_table_end_pattern = r"\\end{tabular}"
+
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
@@ -15,7 +27,7 @@ def get_subfolder_path(
     path: PathLike,
     targets: Sequence[Union[str, Sequence[str]]] = (
         ("*.jpg", "*.png"),
-        ("*.txt"),
+        ("*.txt",),
     ),
 ) -> List[Path]:
     subfolder_paths = list()
@@ -42,3 +54,59 @@ def get_subfolder_path(
                 logger.debug(f"get_subfolder_path: Add {folder!s}")
                 find_paths.append(folder)
     return subfolder_paths
+
+
+def preprocess_latex_table_string(
+    latex_table_str: str,
+) -> str:
+    processed_latex_table_str = re.sub(_latex_table_begin_pattern, "", latex_table_str)
+    processed_latex_table_str = re.sub(_latex_table_end_pattern, "", processed_latex_table_str)
+    processed_latex_table_str = processed_latex_table_str.replace("\n", " ").strip()
+
+    # Filter multi \hline error
+    rows = processed_latex_table_str.split(r"\\")
+    new_rows = list()
+    for row in rows:
+        _row = row
+        if row.count(r"\hline") > 1:
+            _row = _row.replace(r"\hline", "").strip()
+            _row = rf"\hline {_row}"
+        new_rows.append(_row)
+
+    return "\\\\\n".join(new_rows)
+
+
+def pre_check_latex_table_string(
+    latex_table_str: str,
+) -> Tuple[str, str]:
+    results = re.findall(_latex_table_begin_pattern, latex_table_str)
+    if not results:
+        raise NotLatexError("Not latex table")
+    elif "multicolumn" in latex_table_str:
+        raise NotSupportMulticolumnLatexError("Not support convert have multicolumn latex")
+    elif len(results) > 1:
+        raise NotSupportMulticolumnLatexTableError("Not support convert have latex table")
+
+    begin_str = results[0]
+    end_str = r"\end{tabular}"
+    return (begin_str, end_str)
+
+
+def convert_latex_table_to_pandas(
+    latex_table_str: str,
+    headers: Union[bool, Sequence[str], None] = None,
+) -> pd.DataFrame:
+    processed_latex_table_str = preprocess_latex_table_string(latex_table_str)
+    rows = [row.strip() for row in processed_latex_table_str.split(r"\\") if "&" in row]  # 過濾掉無關的行
+
+    # 拆分表格各儲存格
+    table_data = [row.replace(r"\\", "").replace(r"\hline", "").strip().split("&") for row in rows]
+    cleaned_data = [[cell.strip() for cell in row] for row in table_data]
+
+    if isinstance(headers, bool) and headers:
+        headers = cleaned_data[0]  # 第一行是列名
+        data = cleaned_data[1:]  # 剩餘的是數據
+        df = pd.DataFrame(data, columns=headers)
+    elif headers:
+        df = pd.DataFrame(cleaned_data)
+    return df
