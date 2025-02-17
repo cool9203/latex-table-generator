6b1fc180d5bc3003a3583d8ca03fb5a94e4e411c
cool9203 <octer18@gmail.com>
2025-02-17 15:29:43 +0800

    Update: add `remove_all_space_row` in `convert_latex_table_to_pandas`

--- src/latex_table_generator/utils.py
+++ src/latex_table_generator/utils.py
@@ -98,6 +98,7 @@ def convert_latex_table_to_pandas(
     latex_table_str: str,
     headers: Union[bool, Sequence[str], None] = None,
     unsqueeze: bool = False,
+    remove_all_space_row: bool = False,
 ) -> pd.DataFrame:
     pre_check_latex_table_string(latex_table_str=latex_table_str)
     processed_latex_table_str = preprocess_latex_table_string(latex_table_str)
@@ -127,7 +128,10 @@ def convert_latex_table_to_pandas(
                             _row_data.append("")
             else:
                 _row_data.append(cell.strip())
-        cleaned_data.append(_row_data)
+        if remove_all_space_row and len("".join(_row_data)) == 0:
+            continue
+        else:
+            cleaned_data.append(_row_data)
 
     # Process multirow
     for col in range(len(cleaned_data)):
